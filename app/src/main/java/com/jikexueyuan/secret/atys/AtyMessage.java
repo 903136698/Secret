97ba2d00261c838d34999a901c9898ddbead1e35
MiracleWong <cfwr1991@126.com>
2015-07-06 11:48:06 +0800

    完成视频ch24的功能：呈现评论列表界面

--- app/src/main/java/com/jikexueyuan/secret/atys/AtyMessage.java
+++ app/src/main/java/com/jikexueyuan/secret/atys/AtyMessage.java
@@ -1,15 +1,21 @@
 package com.jikexueyuan.secret.atys;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jikexueyuan.secret.Config;
 import com.jikexueyuan.secret.R;
+import com.jikexueyuan.secret.net.Comment;
+import com.jikexueyuan.secret.net.GetComment;
+
+import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -17,7 +23,7 @@ import butterknife.InjectView;
 /**
  * Created by MiracleWong on 2015/7/3.
  */
-public class AtyMessage extends Activity {
+public class AtyMessage extends ListActivity {
 
     @InjectView(R.id.tvMessage)
     TextView tvMessage;
@@ -29,12 +35,17 @@ public class AtyMessage extends Activity {
     Button btnSendComment;
     private String phone_md5, msg, msgId, token;
 
+    private AtyMessageCommentListAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.aty_message);
         ButterKnife.inject(this);
+
+        adapter = new AtyMessageCommentListAdapter(this);
+        setListAdapter(adapter);
 
         Intent data = getIntent();
         phone_md5 = data.getStringExtra(Config.KEY_PHONE_MD5);
@@ -43,6 +54,31 @@ public class AtyMessage extends Activity {
         token = data.getStringExtra(Config.KEY_TOKEN);
 
         tvMessage.setText(msg);
+
+
+        final ProgressDialog pd = ProgressDialog.show(this, getResources().getString(R.string.connecting), getResources().getString(R.string.connecting_to_server));
+        new GetComment(phone_md5, token, msgId, 1, 20, new GetComment.SuccessCallback() {
+            @Override
+            public void onSuccess(String msgId, int page, int perpage, List<Comment> comments) {
+                pd.dismiss();
+
+                adapter.addAll(comments);
+            }
+        }, new GetComment.FailCallback() {
+            @Override
+            public void onFail(int errorCode) {
+                pd.dismiss();
+
+                if (errorCode==Config.RESULT_STATUS_INVALID_TOKEN) {
+                    startActivity(new Intent(AtyMessage.this, AtyLogin.class));
+                    finish();
+                }else{
+                    Toast.makeText(AtyMessage.this, R.string.fail_to_get_comment, Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+        );
+
     }
 
 
